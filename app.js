55e981cfcb1f04ced8eca491c45b87b6cffabbbf
dainv <daipro9559@gmail.com>
2018-10-29 11:32:03 +0700

    .

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ app.use(bodyParser.urlencoded({
     extended: false
 }))
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
 app.use(methodOverride())
     //Passport
 app.use(passport.initialize())
