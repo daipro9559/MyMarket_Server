a7547f96f8f8ec8dd3abbe33edd5597b3ce32a2a
Dai Nguyen <daipro9559@gmail.com>
2018-10-31 08:07:34 +0700

    .

--- config/conf.js
+++ config/conf.js
@@ -8,4 +8,7 @@ CONFIG.email = "daipro9559@gmail.com"
 CONFIG.passEmail = "dainguyen95"
 CONFIG.code_expiration = 3600*24 //1 day
 CONFIG.image_item_path = 'public/images/items/'
+CONFIG.image_avatar_path = 'public/images/avatars/'
+CONFIG.image_stand_path = 'public/items/stands/'
+
 module.exports = CONFIG
